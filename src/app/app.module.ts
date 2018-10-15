4b495a96b9eb4d2edd7b6916b60cf60f8a410a34
Alexis Gauthier <alexgauth.work@gmail.com>
2018-10-15 16:37:12 +0200

    newapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CvPage } from '../pages/cv/cv';
+import { PresentationPage } from '../pages/presentation/presentation';
+import { PortfolioPage } from '../pages/portfolio/portfolio';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CvPage,
+    PresentationPage,
+    PortfolioPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CvPage,
+    PresentationPage,
+    PortfolioPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PortfolioPage } from '../portfolio/portfolio';
+import { CvPage } from '../cv/cv';
+import { PresentationPage } from '../presentation/presentation';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,16 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToCv() {
+    this.navCtrl.push(CvPage);
+  }
+  goToPortfolio() {
+    this.navCtrl.push(PortfolioPage);
+  }
+  goToPresentation() {
+    this.navCtrl.push(PresentationPage);
   }
 
 }
